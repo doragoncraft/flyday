479e00fc9ae230f203133e7be8fd54be0775097f
Nick <Nick>
2013-12-30 01:39:49 -0500

    Fixed it.

--- src/net/drepic/flyday/Flyday.java
+++ src/net/drepic/flyday/Flyday.java
@@ -1,8 +1,8 @@
 package net.drepic.flyday;
 
-
 import java.util.Calendar;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,19 +21,45 @@ public class Flyday extends JavaPlugin {
 			 
             @EventHandler
             public void onJoin(PlayerJoinEvent event) {
-        		Player player = event.getPlayer();
-        		Calendar c = Calendar.getInstance();
-        		int day = c.get(Calendar.DAY_OF_WEEK);
-        		String rawJoinMessage = Flyday.this.getConfig().getString("message");
-        		String joinMessage = rawJoinMessage.replace("Â", "");
-        		
-        		if (day == Calendar.FRIDAY) {
-        		player.setAllowFlight(true);
-        		player.sendMessage(joinMessage);
-        		}
-        		
-        		
-        	}
+            	Player player = event.getPlayer();
+            	Calendar cal = Calendar.getInstance(); 
+            	int dayOfWeek = cal.get(7);
+            	String day;
+            	switch(dayOfWeek) {
+            	case 1: // '\001' 
+            		day = "sun"; 
+            		break;   
+            	case 2: // '\002' 
+            		day = "mon"; 
+            		break;   
+            	case 3: // '\003' 
+            		day = "tue"; 
+            		break;   
+            	case 4: // '\004' 
+            		day = "wed"; 
+            		break;   
+            	case 5: // '\005' 
+            		day = "thu"; 
+            		break;   
+            	case 6: // '\006' 
+            		day = "fri"; 
+            		break;   
+            	case 7: // '\007' 
+            		day = "sat"; 
+            		break;
+            	default:
+            		day = "default";
+            		break;
+        	    }
+            	if (day.equals("fri")) {
+            		player.setAllowFlight(true);
+            		player.setFlying(true);
+            		player.sendMessage(ChatColor.AQUA+"It's Friday! Enjoy free flying all day!");
+            	} else {
+            		player.setAllowFlight(false);
+            		player.setFlying(false);
+            	}
+            }
 
         }, this);
 
